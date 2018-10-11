6dc0ce5a2af4edba81e9f0b66434561112fc4b00
MaundyMoo <maund007@gmail.com>
2018-10-11 08:45:10 +0000

    Reading image files

--- Constants.py
+++ Constants.py
@@ -1,6 +1,8 @@
 import os
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+#Gets the directory of where the file is being run from
 WORKING_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+#Joins any input path to the working directory
 def getPath(path):
     return os.path.join(WORKING_DIRECTORY, path)
--- Image.py
+++ Image.py
@@ -3,6 +3,8 @@ from PIL import Image as Img
 import Constants
 def ReadImage(path):
     path = CONSTANTS.getPath(path)
+    #Changes any incorrect slashes in the path to correct ones, 
+    #which should allow the code to work on linux as well
     canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
     image = pygame.image.load(path).convert()
     return image
