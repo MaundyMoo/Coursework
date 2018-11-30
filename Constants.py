bcd10cca729481f59762156d8c2fe29136d1b5f9
MaundyMoo <maund007@gmail.com>
2018-11-30 09:02:04 +0000

    Started work on tiles

--- Constants.py
+++ Constants.py
@@ -1,6 +1,9 @@
 import os
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+
+TILESIZE = 32
+
 #Gets the directory of where the file is being run from
 WORKING_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 #Joins any input path to the working directory
---
+++ Tiles.py
@@ -0,0 +1,10 @@
+import Constants, Image
+class Tile:
+    def __init__(self, gridPos: tuple, spritePath: str, collision: bool):
+        spritePath = Constants.getPath(spritePath) 
+        self.sprite = Image.ReadImage(spritePath) 
+        self.x, self.y = gridPos[0], gridPos[1]
+    def isCollidable(self) -> bool:
+        return collision
+    def Render(self, screen):
+        screen.blit(self.sprite, (self.x * Constants.TILESIZE, self.y * Constants.TILESIZE)
