da633bd37a008c5a8ac8ed49fa7d8253edf8306e
MaundyMoo <maund007@gmail.com>
2019-01-23 10:31:17 +0000

    SpriteSheet started

--- Image.py
+++ Image.py
@@ -9,3 +9,30 @@ def ReadImage(path: str):
     canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
     image = pyimg.load(path)
     return image
+
+class SpriteSheet:
+    def __init__(self, path: str, spriteSize: int):
+        self.spriteSize = spriteSize
+        path = Constants.getPath(path)
+        #Open as an image to allow for cropping and selecting individual parts of it later
+        self.sheet = Img.open(path)
+        #Allows me to access the alpha channel which will be necessary later for when I need to remove colours
+        self.sheet = self.sheet.convert('RGBA')
+        self.sheet.close()
+
+    #Returns a list of every sprite on the sheet, although probably could crop the sprites individually instead
+    def getSpriteTiles(self) -> list:
+        sprites = []
+        #Gets a 1D list of pixels
+        pixels = list(self.sheet.getdata())
+        #converts the 1D list into a 2D list
+        width, height = self.sheet.size
+        pixelmap = [pixels[i * width:
+            (i+1) * width] for i in range(height)]
+        for y in range(0, (int(len(pixelmap)) / self.spriteSize)+1):
+            row = []
+            for x in range(0, (int(len(pixelmap[0])) / self.spriteSize)+1):
+                sprite = self.sheet.crop((x * self.spriteSize,y * self.spriteSize,(x * (self.spriteSize+1)),(y * (self.spriteSize+1))))
+                row.append(sprite)
+            sprites.append(row)
+        return sprites
