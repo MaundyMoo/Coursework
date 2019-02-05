7c0d9ec63104a3bfbcfe0fe694c61ec74dd31a15
MaundyMoo <maund007@gmail.com>
2019-02-05 14:46:36 +0000

    Adding transparency method to spritesheet

--- Image.py
+++ Image.py
@@ -18,6 +18,7 @@ class SpriteSheet:
         self.sheet = Img.open(path)
         #Allows me to access the alpha channel which will be necessary later for when I need to remove colours
         self.sheet = self.sheet.convert('RGBA')
+        self.removeColour(list(self.sheet.getdata()))
         self.spriteMap = self.getSpriteTiles()
         self.sheet.close()
 
@@ -38,6 +39,18 @@ class SpriteSheet:
                 row.append(sprite)
             sprites.append(row)
         return sprites
+
+    # Removes certain colours from the spritesheet #FF00FF, #880088 are removed
+    def removeColour(self, image):
+        width, height = self.sheet.size
+        image = [image[i * width:
+            (i + 1) * width] for i in range(height)]
+        for y in range(0,len(image)):
+            for x in range(0, len(image[0])):
+                #If the detected colour is #FF00FF or #880088, replace it with a transparent white pixel
+                if image[y][x] == (255, 0, 255, 255) or image[y][x] == (136, 0, 136, 255):
+                    self.sheet.putpixel((x,y), (255, 255, 255, 0))
+
     def  returnSprite(self, x: int, y: int):
         return self.ImageToSprite(self.spriteMap[y][x])
 
